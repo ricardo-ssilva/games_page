de4e1a1989bcd760d7d0c56068a862b1712ecdd5
Ricardo Silva <ricardo.ssilva197@gmail.com>
2022-02-26 19:10:02 -0300

    adiciona funções aos botões de scroll

---
+++ main.js
@@ -0,0 +1,14 @@
+function scroll1() {
+    document.querySelector('.games1').scrollTo({
+        top: 0,
+        left:0,
+        behavior: ""
+    })
+}
+function scroll2() {
+    document.querySelector('.games1').scrollTo({
+        top: 0,
+        left:712,
+        behavior: "smooth"
+    })
+}
