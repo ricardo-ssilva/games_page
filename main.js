1f8cb1dbc1a0921f40586dcb607b5ba42a23e81a
Ricardo Silva <ricardo.ssilva197@gmail.com>
2022-02-27 00:52:28 -0300

    finaliza o visual pagina, ainda não responsiva

--- main.js
+++ main.js
@@ -1,14 +1,57 @@
+
 function scroll1() {
     document.querySelector('.games1').scrollTo({
         top: 0,
-        left:0,
-        behavior: ""
+        left:  0,
+        behavior: "smooth"
     })
 }
 function scroll2() {
     document.querySelector('.games1').scrollTo({
         top: 0,
-        left:712,
+        left: 756,
+        behavior: "smooth"
+    })
+}
+function scroll3() {
+    document.querySelector('.games2').scrollTo({
+        top: 0,
+        left:  0,
+        behavior: "smooth"
+    })
+}
+function scroll4() {
+    document.querySelector('.games2').scrollTo({
+        top: 0,
+        left: 756,
+        behavior: "smooth"
+    })
+}
+function scroll5() {
+    document.querySelector('.games3').scrollTo({
+        top: 0,
+        left:  0,
+        behavior: "smooth"
+    })
+}
+function scroll6() {
+    document.querySelector('.games3').scrollTo({
+        top: 0,
+        left: 756,
+        behavior: "smooth"
+    })
+}
+function scroll7() {
+    document.querySelector('.games4').scrollTo({
+        top: 0,
+        left:  0,
+        behavior: "smooth"
+    })
+}
+function scroll8() {
+    document.querySelector('.games4').scrollTo({
+        top: 0,
+        left: 756,
         behavior: "smooth"
     })
 }
